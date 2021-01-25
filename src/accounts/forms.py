53e7614c4f70a92e8da5aa66b8d0124b01fe2943
markdrrr <markdrrr@gmail.com>
2021-01-25 14:49:04 +0300

    Регистрация и настройка пользователя

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.hashers import check_password
 
+from scraping.models import City, Language
+
 User = get_user_model()
 
 
@@ -23,3 +25,41 @@ class UserLoginForm(forms.Form):
             if not user:
                 raise forms.ValidationError('Данный аккаунт отключен')
         return super(UserLoginForm, self).clean(*args, **kwargs)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    email = forms.EmailField(label='Введите имэйл',
+                             widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Введите пароль',
+                               widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Введите пароль ещё раз',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data['password'] != data['password2']:
+            raise forms.ValidationError('Пароли не совпадают!')
+        return data['password2']
+
+
+class UserUpdateForm(forms.Form):
+    class Meta:
+        model = User
+        fields = ('city', 'language', 'send_email')
+
+    city = forms.ModelChoiceField(
+        queryset=City.objects.all(), to_field_name="slug", required=True,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Город'
+    )
+    language = forms.ModelChoiceField(
+        queryset=Language.objects.all(), to_field_name="slug", required=True,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Специальность'
+    )
+    send_email = forms.BooleanField(required=False, widget=forms.CheckboxInput,
+                                    label='Получать рассылку?')
