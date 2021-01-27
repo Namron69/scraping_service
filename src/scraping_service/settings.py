f3a5e7c38afefcdeb666d1a4d3fcf0eff8fdcb3e
markdrrr <markdrrr@gmail.com>
2021-01-27 18:37:28 +0300

    Рефактор фронта

--- src/scraping_service/settings.py
+++ src/scraping_service/settings.py
@@ -131,3 +131,6 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
